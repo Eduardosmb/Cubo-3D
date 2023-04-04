f922f58fce227b2a523feb1e363f2e920703b58d
Eduardosmb <you@example.com>
2023-04-04 15:48:06 -0300

    cubo incial

--- cubo.py
+++ cubo.py
@@ -1,2 +1,82 @@
 import pygame 
-import numpy as np
+import numpy
+clock = pygame.time.Clock()
+
+
+#faca o setup incial do pygame 
+pygame.init()
+
+#definir a largura e altura da tela
+width, height = 600, 800
+
+FPS = 60
+
+#criar a tela
+screen = pygame.display.set_mode((height, width))
+
+rodando = True
+while rodando:
+    # Capturar eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+
+    # Controlar frame rate
+    clock.tick(FPS)
+
+    # Desenhar
+    # desenhar uma linha
+
+    pygame.draw.line(screen, (255, 0, 0), (200, 500), (500, 500), 5)
+    pygame.draw.line(screen, (255, 0, 0), (200, 500), (200, 200), 5)
+    pygame.draw.line(screen, (255, 0, 0), (500, 500), (500, 200), 5)
+    pygame.draw.line(screen, (255, 0, 0), (200, 200), (500, 200), 5)
+
+    pygame.draw.line(screen, (255, 0, 0), (250, 250), (450, 250), 5)
+    pygame.draw.line(screen, (255, 0, 0), (250, 250), (250, 450), 5)
+    pygame.draw.line(screen, (255, 0, 0), (250, 450), (450, 450), 5)
+    pygame.draw.line(screen, (255, 0, 0), (450, 250), (450, 450), 5)
+
+
+    pygame.draw.line(screen, (255, 0, 0), (250, 250), (250, 450), 5)
+    pygame.draw.line(screen, (255, 0, 0), (250, 250), (250, 450), 5)
+    pygame.draw.line(screen, (255, 0, 0), (250, 250), (250, 450), 5)
+    pygame.draw.line(screen, (255, 0, 0), (250, 250), (250, 450), 5)
+
+    pygame.draw.line(screen, (255, 0, 0), (200, 500), (250, 450), 5)
+    pygame.draw.line(screen, (255, 0, 0), (250, 250), (200, 200), 5)
+    pygame.draw.line(screen, (255, 0, 0), (450, 250), (500, 200), 5)
+    pygame.draw.line(screen, (255, 0, 0), (450, 450), (500, 500), 5)
+
+
+
+    
+
+
+
+
+
+
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+    # pygame.draw.line(screen, (255, 0, 0), (0, 0), (0, 0), 5)
+
+# (-0.5, -0.5, 0.5)
+# (0.5, -0.5, 0.5)
+# (-0.5, 0.5, 0.5)
+# (0.5, 0.5, 0.5)
+# (-0.5, -0.5, -0.5)
+# (0.5, -0.5, -0.5)
+# (-0.5, 0.5, -0.5)
+# (0.5, 0.5, -0.5)
+
+    # Update!
+    pygame.display.update()
+
+# Terminar tela
+pygame.quit()
