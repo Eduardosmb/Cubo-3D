f1756b764d904e4c8da8d648d641b633befdc5fd
Eduardosmb <edubarros253@gmail.com>
2023-04-05 20:52:21 -0300

    cubo

--- cubo.py
+++ cubo.py
@@ -21,10 +21,8 @@ FPS = 60
 screen = pygame.display.set_mode((height, width))
 rodando = True
 
-apertou = False
-
 angulo_x = 0.0  # ângulo de rotação em torno do eixo x
-velocidade_angular_x = 0.003  # velocidade angular constante
+velocidade_angular_x = 0.001  # velocidade angular constante
 
 velocida_parado = 0.0
 
@@ -37,11 +35,11 @@ while rodando:
 
     #se apertar a tecla I o cubo para de rotacionar
     if pygame.key.get_pressed()[pygame.K_i]:
-        velocida_parado = angulo_x
-        angulo_x = 0.0
-        apertou = True
+        velocidade_angular_x = 0.0
 
-
+    #se apertar a tecla O o cubo volta a rotacionar
+    if pygame.key.get_pressed()[pygame.K_o]:
+        velocidade_angular_x = 0.001
 
     #se clicar a tecka w rotaciona o eixo x
     if pygame.key.get_pressed()[pygame.K_m]:
@@ -51,50 +49,42 @@ while rodando:
     if pygame.key.get_pressed()[pygame.K_w]:
         angulo_x = velocidade_angular_x
         Matriz_R = np.array([[1,0,0,0],[0,np.cos(angulo_x),-np.sin(angulo_x),0],[0,np.sin(angulo_x),np.cos(angulo_x),0],[0,0,0,1]])
-        apertou = True
 
     #se clicar a tecka s rotaciona o eixo y
     if pygame.key.get_pressed()[pygame.K_s]:
         angulo_x = - velocidade_angular_x
         Matriz_R = np.array([[1,0,0,0],[0,np.cos(angulo_x),-np.sin(angulo_x),0],[0,np.sin(angulo_x),np.cos(angulo_x),0],[0,0,0,1]])
-        apertou = True
     
     #se clicar a tecka a rotaciona o eixo z
     if pygame.key.get_pressed()[pygame.K_q]:
         angulo_x = velocidade_angular_x
         Matriz_R = np.array([[np.cos(angulo_x),-np.sin(angulo_x),0,0],[np.sin(angulo_x),np.cos(angulo_x),0,0],[0,0,1,0],[0,0,0,1]])
-        apertou = True
     
     #se clicar a tecka d rotaciona o eixo z
     if pygame.key.get_pressed()[pygame.K_e]:
         angulo_x = - velocidade_angular_x
         Matriz_R = np.array([[np.cos(angulo_x),-np.sin(angulo_x),0,0],[np.sin(angulo_x),np.cos(angulo_x),0,0],[0,0,1,0],[0,0,0,1]])
-        apertou = True
 
     #se clicar a tecka q rotaciona o eixo z
     if pygame.key.get_pressed()[pygame.K_a]:
         angulo_x = velocidade_angular_x
         Matriz_R = np.array([[np.cos(angulo_x),0,np.sin(angulo_x),0],[0,1,0,0],[-np.sin(angulo_x),0,np.cos(angulo_x),0],[0,0,0,1]])
-        apertou = True
     
     #se clicar a tecka e rotaciona o eixo z
     if pygame.key.get_pressed()[pygame.K_d]:
         angulo_x = - velocidade_angular_x
         Matriz_R = np.array([[np.cos(angulo_x),0,np.sin(angulo_x),0],[0,1,0,0],[-np.sin(angulo_x),0,np.cos(angulo_x),0],[0,0,0,1]])
-        apertou = True
     
     # se clicar a tecla g dimiui a distancia focal
     if pygame.key.get_pressed()[pygame.K_g]:
         if distancia_focal > 1:
             distancia_focal -= 1
             matriz_aux = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,distancia_focal],[0,0,-1/distancia_focal,0]])
-            apertou = True
-    
+        
     # se clicar a tecla h aumenta a distancia focal
     if pygame.key.get_pressed()[pygame.K_h]:
         distancia_focal += 1
         matriz_aux = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,distancia_focal],[0,0,-1/distancia_focal,0]])
-        apertou = True
 
 
     # Transladar o cubo para o centro da tela
@@ -130,7 +120,6 @@ while rodando:
     pygame.draw.line(screen,(cor),(matriz_resultante[0,4]/matriz_resultante[3,4], matriz_resultante[1,4]/matriz_resultante[3,4]),(matriz_resultante[0,5]/matriz_resultante[3,5], matriz_resultante[1,5]/matriz_resultante[3,5]),width=5)
     pygame.draw.line(screen,(cor),(matriz_resultante[0,6]/matriz_resultante[3,6], matriz_resultante[1,6]/matriz_resultante[3,6]),(matriz_resultante[0,7]/matriz_resultante[3,7], matriz_resultante[1,7]/matriz_resultante[3,7]),width=5)
     pygame.draw.line(screen,(cor),(matriz_resultante[0,5]/matriz_resultante[3,5], matriz_resultante[1,5]/matriz_resultante[3,5]),(matriz_resultante[0,7]/matriz_resultante[3,7], matriz_resultante[1,7]/matriz_resultante[3,7]),width=5)
-    apertou = False
 
 
 
